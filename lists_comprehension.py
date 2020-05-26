9713b266932406062ce7b3a979df72018b4c6ce0
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-26 16:33:58 +0530

    Add files via upload

---
+++ lists_comprehension.py
@@ -0,0 +1,9 @@
+cities = ["Jaipur","Hyderabad","Bombay","Delhi","Bangalore","Surat","Guwahati"]
+
+cities2 = [city.upper() for city in cities]
+print(cities2)
+
+primes = [2,3,5,7,11,13,17,19]
+
+primes2 = [x*x for x in primes]
+print(primes2)
