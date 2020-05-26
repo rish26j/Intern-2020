561afb2953d669d0e6411e0a3b8f7f990354a98c
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-26 16:17:27 +0530

    Add files via upload

---
+++ lists_swapping.py
@@ -0,0 +1,11 @@
+cities = ["Jaipur","Hyderabad","Bombay","Delhi","Bangalore","Surat","Guwahati"]
+
+cities[4],cities[6] = cities[6],cities[4]
+
+print(cities)
+
+cities.sort()
+print(cities)
+
+cities.reverse()
+print(cities)
