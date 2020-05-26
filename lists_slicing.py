ed15866ee2ee971c594ea02565d370bcf2af0050
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-26 16:10:41 +0530

    Add files via upload

---
+++ lists_slicing.py
@@ -0,0 +1,12 @@
+cities = ["Jaipur","Hyderabad","Bombay","Delhi","Bangalore","Surat"]
+print(cities[::2])
+
+print(cities[2:])
+
+print(cities[:2])
+
+print(cities[1:4])
+
+print(cities[3::-1])
+
+print(cities[:3:-1])
