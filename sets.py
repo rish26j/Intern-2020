f2125ab75389760aa0ffa295128a9f26ea36dfae
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-28 12:22:04 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,10 @@
+random_numbers = {1,2,3,2,4,5,4,6,3,6,8,9,5,3,2,4,2}
+
+print(random_numbers)
+
+print(len(random_numbers))
+
+print(4 in random_numbers)
+
+for num in random_numbers:
+    print(num**2)
