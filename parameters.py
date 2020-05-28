a0ca6dbb720a2e11751c778a75adb315887cc507
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-28 14:03:56 +0530

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,13 @@
+def happy_birthday(name):
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print(f"Happy birthday dear {name}")
+    print("Happy birthday to you")
+
+happy_birthday("28th May")
+
+def division(num1,num2):
+    return num1/num2
+
+print(division(5,2))
+print(division(2,5))
