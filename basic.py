e6ed9a02f6f318ac12410e53abb7f2dbc23129bd
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-28 13:36:12 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,14 @@
+def happy_birthday():
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print("Happy birthday dear xyz")
+    print("Happy birthday to you")
+
+happy_birthday()
+
+print("\n")
+
+def square_of_10():
+    return 10**2
+
+print(square_of_10())
