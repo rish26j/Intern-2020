04d4efadccca910b595d0f33b37868d6c9d3b04d
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-28 15:09:15 +0530

    Add files via upload

---
+++ documentation.py
@@ -0,0 +1,8 @@
+def power(num,pow=2):
+    """The power funtion raises the first argument to the power of second. 2 is a default value of power"""
+    return num**pow
+
+print.__doc__
+
+print(power(3,4))
+print(power(6))
