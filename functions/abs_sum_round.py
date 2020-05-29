43caa1241a125d5e78d41349b4b2b0777115f2c7
rish26j <65274152+rish26j@users.noreply.github.com>
2020-05-29 14:43:59 +0530

    Add files via upload

---
+++ functions/abs_sum_round.py
@@ -0,0 +1,13 @@
+num = 4.67
+print(abs(num))
+num2 = -4.67
+print(abs(num2))
+
+summing = sum([1,3,5,8])
+print(summing)
+
+rounds = round(3.65949586)
+print(rounds)
+
+round2= round(4.9284454, 2)
+print(round2)
