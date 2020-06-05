69e7082bacb91ec67aba21c7306c16b9ad9f77bd
rish26j <65274152+rish26j@users.noreply.github.com>
2020-06-05 13:12:07 +0530

    Add files via upload

---
+++ database_access/table.py
@@ -0,0 +1,19 @@
+import pymysql
+
+db = pymysql.connect("localhost","testuser","testxyz")
+
+cursor = db.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+# Create table as per requirement
+sql = """CREATE TABLE EMPLOYEE (
+   FIRST_NAME  CHAR(20) NOT NULL,
+   LAST_NAME  CHAR(20),
+   AGE INT,  
+   SEX CHAR(1),
+   INCOME FLOAT )"""
+
+cursor.execute(sql)
+
+db.close()
