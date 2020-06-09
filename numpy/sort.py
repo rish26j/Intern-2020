562ff713387b1a4bd99855401133e1993b1be552
rish26j <65274152+rish26j@users.noreply.github.com>
2020-06-09 16:16:02 +0530

    Add files via upload

---
+++ numpy/sort.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr=np.array([[[1,2,9],[3,4,49]],[[5,6,121],[7,8,225]]])
+
+print(np.sort(arr))
+
+cities=np.array(["Jaipur","Bangalore","Delhi","Bombay","Madras","Calcutta"])
+print(np.sort(cities))
+
+
