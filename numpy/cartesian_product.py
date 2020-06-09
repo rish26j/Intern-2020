b2e3ba8bb0ff211da2de285712c42cb1a6ccbf4c
rish26j <65274152+rish26j@users.noreply.github.com>
2020-06-09 14:51:49 +0530

    Add files via upload

---
+++ numpy/cartesian_product.py
@@ -0,0 +1,13 @@
+def cartesian(arrays):
+    arrays = [np.asarray(a) for a in arrays]
+    shape = (len(x) for x in arrays)
+
+    ix = np.indices(shape, dtype=int)
+    ix = ix.reshape(len(arrays), -1).T
+
+    for n, arr in enumerate(arrays):
+        ix[:, n] = arrays[n][ix[:, n]]
+
+    return ix
+
+print (cartesian(([1, 2, 3], [4, 5], [6, 7])))
