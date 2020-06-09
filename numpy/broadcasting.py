c5f59234b3cbae3baa87cf15c393fa8e07da078b
rish26j <65274152+rish26j@users.noreply.github.com>
2020-06-09 16:55:42 +0530

    Add files via upload

---
+++ numpy/broadcasting.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1=np.array([[1,2,3],[4,5,6],[7,8,9]])
+array2=np.array([1,0,1])
+array=array1+array2
+print(array)
+
+w=np.array([[1],[0],[1]])
+array=array1+array2
+print(array)
