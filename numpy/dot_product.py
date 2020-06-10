eb36bd7f75456314434900bfd143b25a47f0627f
rish26j <65274152+rish26j@users.noreply.github.com>
2020-06-10 15:56:07 +0530

    Add files via upload

---
+++ numpy/dot_product.py
@@ -0,0 +1,11 @@
+import numpy as geek
+
+# Scalars
+product = geek.dot(5, 4)
+print("Dot Product of scalar values  : ", product)
+
+vector_a = 2 + 3j
+vector_b = 4 + 5j
+
+product = geek.dot(vector_a, vector_b)
+print("Dot Product  : ", product)
